6aa4dc4715d2a59ffb6e995ad2ad0c602ba3a018
logang5764 <logangarside@gmail.com>
2026-04-18 19:24:40 -0400

    draw map

--- main.c
+++ main.c
@@ -1,54 +1,70 @@
 #include <stdio.h>
-
 #include "raylib.h"
 
-//====================
-//
-//  Compile using:
-//
-//  gcc <file.name> -o <new_file.name> -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-//
-//====================
+// Screen dimensions
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 500
 
+// Map size
 #define ROWS 10
 #define COLS 10
 
+// Cell size based on dimension and size
+#define CELL_WIDTH (SCREEN_WIDTH / COLS)
+#define CELL_HEIGHT (SCREEN_HEIGHT / ROWS)
+
+
+// Map
 int map[ROWS][COLS] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+    {1, 0, 1, 0, 1, 1, 1, 1, 0, 1},
+    {1, 0, 1, 1, 1, 0, 1, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 1, 0, 1, 1},
+    {1, 0, 1, 0, 1, 1, 1, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 1, 0, 1, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
 
+int main(void)
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Map");
+    SetTargetFPS(60);
 
-
-int main(void) {
-    InitWindow(500, 500, "First Window");
-
-
-    for (int y = 0; y < ROWS; y++) {
-        printf("\nRow Number: %d \n", y);
-        for (int x = 0; x < COLS; x++) {
-            printf("Column Number: %d ", x);
-        };
-        printf("\n");
-    };
-
-
-
-
-    while (!WindowShouldClose()) {
+    while (!WindowShouldClose())
+    {
         BeginDrawing();
-            ClearBackground(BLACK); 
+        ClearBackground(BLACK);
+
+        // Draw map
+        for (int y = 0; y < ROWS; y++)
+        {
+            for (int x = 0; x < COLS; x++)
+            {
+                // Position on drawing
+                int posX = x * CELL_WIDTH;
+                int posY = y * CELL_HEIGHT;
+
+                Color tileColor;
+
+                // Find colour
+                if (map[y][x] == 1)
+                {
+                    tileColor = DARKGRAY;
+                }
+                else
+                {
+                    tileColor = BLACK;
+                };
+
+                DrawRectangle(posX, posY, CELL_WIDTH, CELL_HEIGHT, tileColor);
+            }
+        }
         EndDrawing();
     }
 
     CloseWindow();
     return 0;
-};
+}
