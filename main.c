2ee0b55d21c313dd79b2f86a08f433b8138769ee
logang5764 <logangarside@gmail.com>
2026-04-18 19:53:32 -0400

    make program modular

--- main.c
+++ main.c
@@ -3,16 +3,15 @@
 
 // Screen dimensions
 #define SCREEN_WIDTH 1000
-#define SCREEN_HEIGHT 500
+#define SCREEN_HEIGHT 1000
 
 // Map size
 #define ROWS 10
 #define COLS 10
 
-// Cell size based on dimension and size
+// Cell size
 #define CELL_WIDTH (SCREEN_WIDTH / COLS)
 #define CELL_HEIGHT (SCREEN_HEIGHT / ROWS)
-
 
 // Map
 int map[ROWS][COLS] = {
@@ -28,40 +27,31 @@ int map[ROWS][COLS] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
 
-int main(void)
-{
+// --- FUNCTIONS ---
+void DrawWorldMap(void) {
+    for (int y = 0; y < ROWS; y++) {
+        for (int x = 0; x < COLS; x++) {
+            int posX = x * CELL_WIDTH;
+            int posY = y * CELL_HEIGHT;
+
+            Color tileColor = (map[y][x] == 1) ? DARKGRAY : BLACK;
+
+            DrawRectangle(posX, posY, CELL_WIDTH, CELL_HEIGHT, tileColor);
+        }
+    }
+}
+
+// --- MAIN ---
+int main(void) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Map");
     SetTargetFPS(60);
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(BLACK);
+            ClearBackground(BLACK);
 
-        // Draw map
-        for (int y = 0; y < ROWS; y++)
-        {
-            for (int x = 0; x < COLS; x++)
-            {
-                // Position on drawing
-                int posX = x * CELL_WIDTH;
-                int posY = y * CELL_HEIGHT;
+            DrawWorldMap();
 
-                Color tileColor;
-
-                // Find colour
-                if (map[y][x] == 1)
-                {
-                    tileColor = DARKGRAY;
-                }
-                else
-                {
-                    tileColor = BLACK;
-                };
-
-                DrawRectangle(posX, posY, CELL_WIDTH, CELL_HEIGHT, tileColor);
-            }
-        }
         EndDrawing();
     }
 
